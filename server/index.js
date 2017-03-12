4caaa24dbbe6e476c1cebca11ea049503b8b17e0
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-12 21:22:57 +0000

    Create basic node application

--- server/index.js
+++ server/index.js
@@ -1,1 +1,36 @@
 //node stuff
+// var Counter = require('../src/models/counter')
+
+var express = require('express')
+var bodyParser = require('body-parser');
+var cors = require('cors')
+var app = express()
+var candidates = [
+  {
+    name: "Candidate-1",
+    votes: 10
+  },
+  {
+    name: "Candidate-2",
+    votes: 20
+  },
+]
+
+
+app.use(bodyParser.json());
+app.use(cors())
+
+//post some votes
+app.post("/votes", function(req, res){
+  console.log(req.body[0])
+  res.json({})
+})
+
+//get voting page
+app.get("/votes", function(req, res){
+  res.json(candidates)
+})
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000")
+})
